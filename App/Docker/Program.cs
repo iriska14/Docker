3dcca82e4cd14bb0c783f968d9d3aa85bbe91dcb
iriska14 <104430601+iriska14@users.noreply.github.com>
2023-10-14 15:30:24 +0700

    Add files via upload

---
+++ App/Docker/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int [] { 1, 2, 3 };
+            var srt = string.Join (" ", array);
+            Console.WriteLine(srt);
+        }
+    }   
+}
